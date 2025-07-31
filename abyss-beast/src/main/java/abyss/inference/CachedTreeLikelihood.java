19e17eb6eded50cf90bb7d83a26127d9a20a43d9
jasminesgh <jsag090@aucklanduni.ac.nz>
2025-07-31 17:43:35 +1200

    rm redundant part

--- abyss-beast/src/main/java/abyss/inference/CachedTreeLikelihood.java
+++ abyss-beast/src/main/java/abyss/inference/CachedTreeLikelihood.java
@@ -1,9 +1,6 @@
 package abyss.inference;
 
 import beast.base.evolution.likelihood.TreeLikelihood;
-import beast.pkgmgmt.BEASTClassLoader;
-
-import java.lang.reflect.InvocationTargetException;
 
 public class CachedTreeLikelihood extends TreeLikelihood {
 
@@ -12,47 +9,23 @@ public class CachedTreeLikelihood extends TreeLikelihood {
     @Override
     public void initAndValidate() {
         super.initAndValidate();
-        try {
-            Object o = newTreeLikelihood();
-            if (o instanceof CachedBeagleTreeLikelihood) {
-                beagle = (CachedBeagleTreeLikelihood) o;
-            }
-            beagle.initByName(
-                    "data", dataInput.get(), "tree", treeInput.get(), "siteModel", siteModelInput.get(),
-                    "branchRateModel", branchRateModelInput.get(), "useAmbiguities", m_useAmbiguities.get(),
-                    "useTipLikelihoods", m_useTipLikelihoods.get(),"scaling", scaling.get().toString(),
-                    "rootFrequencies", rootFrequenciesInput.get());
-            if (beagle.getBeagle() != null) {
-                //a Beagle instance was found, so we use it
-                return;
-            }
-        } catch (Exception e) {
-            // ignore
-        }
-        // No Beagle instance was found, so we use java
-        beagle = null;
-
         storedPatternLogLikelihoods = new double[alignment.getPatternCount()];
     }
 
     @Override
     public void store() {
         super.store();
+        if (beagle != null) return;
         System.arraycopy(patternLogLikelihoods, 0, storedPatternLogLikelihoods, 0, patternLogLikelihoods.length);
     }
 
     @Override
     public void restore() {
         super.restore();
+        if (beagle != null) return;
         double[] tmp = patternLogLikelihoods;
         patternLogLikelihoods = storedPatternLogLikelihoods;
         storedPatternLogLikelihoods = tmp;
-    }
-
-    private TreeLikelihood newTreeLikelihood() throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, ClassNotFoundException {
-        // must set implementationInput to CachedBeagleTreeLikelihood to use beagle
-        String class_ = implementationInput.get();
-        return (TreeLikelihood) BEASTClassLoader.forName(class_).getConstructor().newInstance();
     }
 
 }
