1b446532e1fe529dd2b8b4389c365fb0e5b55e34
jasminesgh <jsag090@aucklanduni.ac.nz>
2025-07-31 17:44:58 +1200

    still bugged, smth to fix for use with beagle

--- abyss-beast/src/main/java/abyss/inference/CachedBeagleTreeLikelihood.java
+++ abyss-beast/src/main/java/abyss/inference/CachedBeagleTreeLikelihood.java
@@ -4,28 +4,29 @@ import beast.base.evolution.likelihood.BeagleTreeLikelihood;
 
 public class CachedBeagleTreeLikelihood extends BeagleTreeLikelihood {
 
-    protected double[] storedPatternLogLikelihoods;
+//    protected double[] storedPatternLogLikelihoods;
 
     @Override
     public void initAndValidate() {
         super.initAndValidate();
-        if (patternLogLikelihoods == null) patternLogLikelihoods = new double[patternCount];
-        beagle.getSiteLogLikelihoods(patternLogLikelihoods);
-        storedPatternLogLikelihoods = new double[alignment.getPatternCount()];
+//        if (patternLogLikelihoods == null) patternLogLikelihoods = new double[patternCount];
+//        beagle.getSiteLogLikelihoods(patternLogLikelihoods);
+//        storedPatternLogLikelihoods = new double[alignment.getPatternCount()];
     }
 
     @Override
     public void store() {
         super.store();
-        System.arraycopy(patternLogLikelihoods, 0, storedPatternLogLikelihoods, 0, patternLogLikelihoods.length);
+//        System.arraycopy(patternLogLikelihoods, 0, storedPatternLogLikelihoods, 0, patternLogLikelihoods.length);
     }
 
     @Override
     public void restore() {
         super.restore();
-        double[] tmp = patternLogLikelihoods;
-        patternLogLikelihoods = storedPatternLogLikelihoods;
-        storedPatternLogLikelihoods = tmp;
+//        requiresRecalculation();
+//        double[] tmp = patternLogLikelihoods;
+//        patternLogLikelihoods = storedPatternLogLikelihoods;
+//        storedPatternLogLikelihoods = tmp;
     }
 
 }
